788a2ef317bbc6fcf0b2b6fee58f6ed8c7e7ac1b
Robin Novak <rnovak@g.clemson.edu>
2013-03-11 16:36:42 -0400

    the php
    i swear this should work

--- phptest.php
+++ phptest.php
@@ -1,5 +1,10 @@
-<? php
-
-echo "whatever"
-
+<?php $body = "<h1>Hello CPSC472/672!</h1>
+<p>This is the default web page for this server.</p>
+<p>The web server software is running but no content has been added yet.</p>"
 ?>
+
+<html>
+  <body>
+  <?php echo $body ?>
+  </body>
+</html>
