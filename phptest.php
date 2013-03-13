87b1e9d6e82ac83d778a59e9fa3922dd9ae854fe
Robin Novak <rnovak@g.clemson.edu>
2013-03-13 16:57:35 -0400

    phptest (testing db)
    now that pushes should work correctly, adding a query result echo for db
    connection that probably wont work

--- phptest.php
+++ phptest.php
@@ -24,6 +24,10 @@ else{
 echo "CONNECTED TO MYSQL";
 }
 
+$queryresult = mysqli_query($connection, "SELECT * FROM Mentor");
+
+echo mysqli_fetch_array($queryresult);
+
 mysqli_close($connection);
 ?>
 
