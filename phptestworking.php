ca16e7e27b4d0583872d5376ffed9c665cd87f56
Robin Novak <rnovak@g.clemson.edu>
2013-03-13 17:21:19 -0400

    new php testing file
    is now a hello world

--- phptestworking.php
+++ phptestworking.php
@@ -1,16 +1,3 @@
-<?php $body = "<h1>Hello CPSC472/672!</h1>
-<p>This is the default web page for this server.</p>
-?>
-
-<html>
-  <body>
-  <?php echo $body ?>
-
-
 <?php
-echo "TIME TO WORK";
-echo "PLEASSEEEE";
+echo "Hello World!";
 ?>
-
-  </body>
-</html>
