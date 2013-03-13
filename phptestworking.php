2b86b257104399ef919cb9abdee956346be06afc
Robin Novak <rnovak@g.clemson.edu>
2013-03-13 17:01:41 -0400

    new php testing file
    if i ever need to verify that the problem is my code and not just
    running an arbitrary php file

---
+++ phptestworking.php
@@ -0,0 +1,16 @@
+<?php $body = "<h1>Hello CPSC472/672!</h1>
+<p>This is the default web page for this server.</p>
+?>
+
+<html>
+  <body>
+  <?php echo $body ?>
+
+
+<? php
+echo "TIME TO WORK";
+echo "PLEASSEEEE";
+?>
+
+  </body>
+</html>
