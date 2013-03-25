007883dbc9bd139f5688e96ccf4bda4cfe661956
Robin Novak <rnovak@g.clemson.edu>
2013-03-25 16:28:50 -0400

    phptest.php edited
    trying to get php file to verify access to db

--- phptest.php
+++ phptest.php
@@ -24,9 +24,11 @@ else{
 echo "CONNECTED TO MYSQL";
 }
 
+/*
 $queryresult = mysqli_query($connection, "SELECT * FROM Mentor");
 
 echo mysqli_fetch_array($queryresult);
+*/
 
 mysqli_close($connection);
 ?>
