e5c2c62ef493853659de542c30967bfe10fdce6f
Robin Novak <rnovak@g.clemson.edu>
2013-03-25 16:37:27 -0400

    phptest.php edited
    verify uploading

--- phptest.php
+++ phptest.php
@@ -1,31 +1,4 @@
-<?php $body = "<h1>Hello CPSC472/672!</h1>
-<p>This is the default web page for this server.</p>
+<?php
+echo "Hello World!";
+echo "Goodbye World!";
 ?>
-
-<html>
-  <body>
-  <?php echo $body ?>
-
-
-<? php
-
-$host = "oss-ci.cs.clemson.edu";
-$dbuser = "myDB4dmin";
-$dbname = "cpsc472";
-$sshuser = "cpsc472";
-$sshpwd = "tigermints";
-
-
-$connection = mysqli_connect($host, $dbuser, $dbpwd, $dbname);
-if (mysqli_connect_errno($connection)){
-	echo "FAILED TO CONNECT TO MYSQL";
-}
-else{
-echo "CONNECTED TO MYSQL";
-}
-
-mysqli_close($connection);
-?>
-
-  </body>
-</html>
