bda438a66f73126aaa875a26912c0a0a7394d56f
Robin Novak <rnovak@g.clemson.edu>
2013-03-25 17:03:34 -0400

    phptest.php edited
    trying to access db

--- phptest.php
+++ phptest.php
@@ -4,28 +4,8 @@
 
 <html>
   <body>
+
   <?php echo $body ?>
-
-
-<? php
-
-$host = "oss-ci.cs.clemson.edu";
-$dbuser = "myDB4dmin";
-$dbname = "cpsc472";
-$sshuser = "cpsc472";
-$sshpwd = "tigermints";
-
-
-$connection = mysqli_connect($host, $dbuser, $dbpwd, $dbname);
-if (mysqli_connect_errno($connection)){
-	echo "FAILED TO CONNECT TO MYSQL";
-}
-else{
-echo "CONNECTED TO MYSQL";
-}
-
-mysqli_close($connection);
-?>
 
   </body>
 </html>
