fd9e2d02e129a0881a212da788caa59b4aefcbdc
Robin Novak <rnovak@g.clemson.edu>
2013-03-25 17:06:01 -0400

    phptest.php edited
    it is now a hello world (again)

--- phptest.php
+++ phptest.php
@@ -1,11 +1,4 @@
-<?php $body = "<h1>Hello CPSC472/672!</h1>
-<p>This is the default web page for this server.</p>
+<?php
+echo "Hello World!";
+echo "Goodbye World!";
 ?>
-
-<html>
-  <body>
-
-  <?php echo $body ?>
-
-  </body>
-</html>
