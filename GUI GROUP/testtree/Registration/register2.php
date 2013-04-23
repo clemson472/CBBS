823ea6aff2ff9bcbd3c4bcc17260983c7f25fbb7
robin novak <rnovak@clemson.edu>
2013-04-23 17:25:28 -0400

    registration
    work on banner

--- GUI GROUP/testtree/Registration/register2.php
+++ GUI GROUP/testtree/Registration/register2.php
@@ -3,7 +3,7 @@
     
     <head>
     		<base href="http://oss-ci.cs.clemson.edu:8080/GUI%20GROUP/testtree/">
-			<link rel="stylesheet media="screen" href="../resources/css/style.css" type"text/css"/>
+			<link rel="stylesheet media="screen" href="resources/css/style.css" type"text/css"/>
     
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>User Registration</title>
